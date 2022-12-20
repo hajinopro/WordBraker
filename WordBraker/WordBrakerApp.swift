ad6a126e668a66e787c237fa783e1d45a3c67381
hajinopro <hajinopro@gmail.com>
2022-12-20 21:19:07 +0900

    Initial Commit

---
+++ WordBraker/WordBrakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBrakerApp.swift
+//  WordBraker
+//
+//  Created by 하진호 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WordBrakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
